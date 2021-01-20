2655c64fd79346fcc14f355400a8601d548ae61e
Odo Rifqi <09021281621065@student.unsri.ac.id>
2021-01-20 10:46:29 +0700

    remove unnecesarry thing

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -11,7 +11,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
